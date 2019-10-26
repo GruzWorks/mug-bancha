1874fcc8173ecc7ead4a9e827f0b325379c1c2f9
outfrost <kotlet.bahn@gmail.com>
2019-10-26 05:48:32 +0200

    Create a basic and somewhat ugly request routing skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use hyper::{Body, Request, Response, Server};
+use hyper::{Server};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
 
 mod route;
 
 fn main() {
 	let addr = ([127, 0, 0, 1], 3000).into();
 
-	let service = || { service_fn_ok(route::route) };
+	let service = || { service_fn(route::dispatch) };
 
 	let server = Server::bind(&addr)
 	                     .serve(service)
+	                     //.into()
 	                     .map_err(|e| eprintln!("server error: {}", e));
 
 	hyper::rt::run(server);
--- src/route.rs
+++ src/route.rs
@@ -1,17 +1,152 @@
-use hyper::{Body, Method, Request, Response, Server};
+use std::collections::HashMap;
 
-pub fn route(request: Request<Body>) -> Response<Body> {
-	match request.uri().path() {
-		"/" => match request.method() {
-			&Method::GET => Response::new(Body::from("INCREDIBLE")),
-			_ => Response::builder()
-			               .status(403)
-			               .body(Body::empty())
-			               .unwrap(),
-		},
-		_ => Response::builder()
-		               .status(404)
-		               .body(Body::empty())
-		               .unwrap(),
+use futures::future;
+use futures::future::{Either, Future};
+use hyper::{Body, Chunk, Method, Request, Response};
+use hyper::rt::Stream;
+use serde::{Serialize, Deserialize, de::DeserializeOwned};
+use serde_json::json;
+
+/**
+ * This is unstable in Rust for now
+type ResponseFuture = impl Future<Item = Response<Body>, Error = hyper::Error>;
+ **/
+type BoxOfDreams = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+enum RequestResult<T> {
+	Ok(T),
+	InvalidPayload,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Endpoint<'a>(&'a str, &'a Method);
+
+struct Resolution {
+	adapter: Box<dyn Fn(Body) -> BoxOfDreams>,
+	sample_request_body: &'static str,
+	sample_response_body: &'static str,
+}
+
+macro_rules! route {
+	( $endpoint:expr,
+	  $handler:expr,
+	  $req_body_type:ty,
+	  $resp_body_type:ty,
+	  $sample_req_body:expr,
+	  $sample_resp_body:expr ) => (
+		( $endpoint, Resolution {
+			adapter: Box::new(|body| -> BoxOfDreams {
+				process_request::<$req_body_type, $resp_body_type>(body, $handler)
+			}),
+			sample_request_body: $sample_req_body,
+			sample_response_body: $sample_resp_body } )
+	)
+}
+
+fn process_request<I, O>(body: Body, handler: &'static (dyn Fn(I) -> RequestResult<O> + Sync))
+		-> BoxOfDreams
+		where I: DeserializeOwned, O: Serialize {
+	let response_future = body.concat2().map(move |chunk| {
+		match serde_json::from_slice::<I>(&chunk) {
+			Ok(input) => {
+				match handler(input) {
+					RequestResult::Ok(output) => {
+						match serde_json::to_string(&output) {
+							Ok(json) => Response::builder()
+							                      .status(200)
+							                      .body(Body::from(json))
+							                      .unwrap(),
+							Err(e) => Response::builder()
+							                    .status(500)
+							                    .body(bodify(json!({ "message": format!("Response serialization failed: {}", e) })))
+							                    .unwrap(),
+						}
+					},
+					_ => Response::builder()
+					               .status(500)
+					               .body(bodify(json!({ "message": "Cannot fulfil request" })))
+					               .unwrap(),
+				}
+			},
+			Err(e) => Response::builder()
+			                    .status(400)
+			                    .body(bodify(json!({ "message": format!("Invalid request payload: {}", e) })))
+			                    .unwrap(),
+		}
+	});
+	Box::new(response_future)
+}
+
+fn bodify(value: serde_json::value::Value) -> Body {
+	Body::from(value.to_string())
+}
+
+
+
+
+#[derive(Serialize)]
+struct EchoResponseBody {
+	message: String,
+}
+
+fn echo(_: ()) -> RequestResult<EchoResponseBody> {
+	RequestResult::Ok(EchoResponseBody { message: String::from("INCREDIBLE") })
+}
+
+
+
+
+struct Router<'a> {
+	routes: HashMap<Endpoint<'a>, Resolution>,
+}
+
+impl<'a> Router<'a> {
+	fn init() -> Self {
+		Self::with_routes(vec![
+			route!(Endpoint("/", &Method::GET),
+			       &echo,
+			       (),
+			       EchoResponseBody,
+			       "",
+			       r#"{"message":"INCREDIBLE"}"#),
+		])
+/*
+		Self::with_routes(vec![
+			(Endpoint("/", Method::GET),
+			 Resolution { adapter: Box::new(|_request| { Response::new(Body::from("INCREDIBLE")) }),
+			              sample_request_body: "",
+			              sample_response_body: "INCREDIBLE" })
+		])
+*/
 	}
+
+	fn with_routes(r: Vec<(Endpoint<'a>, Resolution)>) -> Self {
+		Router { routes: r.into_iter().collect() }
+	}
+
+	fn dispatch_request(&self, request: Request<Body>) -> BoxOfDreams {
+		let (parts, body) = request.into_parts();
+		match self.routes.get(&Endpoint(parts.uri.path(), &parts.method)) {
+			Some(resolution) => (resolution.adapter)(body),
+			None => Box::new(future::ok(Response::builder()
+			                             .status(404)
+			                             .body(bodify(json!({ "message": "Not found" })))
+			                             .unwrap())),
+		}
+	}
+}
+/*
+		Router::with_routes(vec![
+			route!(Endpoint("/", Method::GET),
+			       handler,
+			       InputDataType,
+			       OutputDataType,
+			       "",
+			       "INCREDIBLE")
+		])
+
+*/
+
+pub fn dispatch(request: Request<Body>) -> BoxOfDreams {
+	Router::init().dispatch_request(request)
 }
